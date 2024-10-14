dd757445dbaabead678d912ad3d7d6c64cb9963a
mvcobos <vcobos178@gmail.com>
2024-10-14 12:50:06 -0500

    14/10

--- src/MetodosBusqueda.java
+++ src/MetodosBusqueda.java
@@ -1,25 +1,42 @@
 public class MetodosBusqueda {  
 
     public int busquedaSecuencial(int[] arreglo, int valorBuscar){
+        long startTime = System.nanoTime();
+        System.out.println("Busqueda Secuencial inicia en " + startTime + " nanosegundos");
         for(int i = 0; i < arreglo.length; i++){
             if(arreglo [i] == valorBuscar){
                 System.out.println("BS Comparaciones: " + i);
+                long endTime = System.nanoTime();
+                long tiempoEjecucion = startTime - endTime;
+                System.out.println("Busqueda Secuencial finaliza en " + endTime + " nanosegundos");
+                System.out.println("Busqueda Secuencial tiene una duracion de  " + tiempoEjecucion + " nanosegundos");
+                
                 return i;
             }
             
         }
+        
         return -1;
     }
+
+    
+   
 
     public int busquedaBinaria(int[] arr, int value){
         int inicio = 0;
         int fin = arr.length -1;
         int cont = 1; 
+        long startTime = System.nanoTime();
+        System.out.println("Busqueda Binaria inicia en " + startTime + " nanosegundos");
         while(inicio <= fin){
             int medio = inicio + (fin - inicio) / 2;
             if (arr[medio] == value){
                 //Elemento encontrado
-                System.out.println("BBComparaciones: " + cont);
+                System.out.println("BB Comparaciones: " + cont);
+                long endTime = System.nanoTime();
+                long tiempoEjecucion = startTime - endTime;
+                System.out.println("Busqueda Binaria finaliza en " + endTime + " nanosegundos");
+                System.out.println("Busqueda Binaria tiene una duracion de  " + tiempoEjecucion + " nanosegundos");
                 return medio;
             }
             if (arr[medio] < value){
